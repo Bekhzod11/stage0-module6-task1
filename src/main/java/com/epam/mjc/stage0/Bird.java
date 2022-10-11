0b407e8a604adec0c1815534bda64d9a46321b9e
Bekhzod <Bekhzod1411@gmail.com>
2022-10-11 19:45:33 +0500

    finished
    finished

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,13 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal {
 
+    public Bird(){
+        super("Blue",2, false);
+    }
+
+    @Override
+    public String fetDescription() {
+        return super.fetDescription() + " Moreover, it has 2 wings and can fly.";
+    }
 }
--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -1,5 +1,12 @@
 package com.epam.mjc.stage0;
 
-public class Dog {
+public class Dog extends Animal  {
+    public Dog(){
+        super("Brown",4,true);
+    }
 
+    @Override
+    public String fetDescription() {
+        return super.fetDescription();
+    }
 }
